972d6cb577b9d852b121b29875f7143a02936dec
Dima-Lyty <132125088+Dima-Lyty@users.noreply.github.com>
2023-05-20 12:14:29 +0300

    xe

--- main.py
+++ main.py
@@ -74,11 +74,21 @@
 #
 
 
+def checker(*exc_types):
+    def checker(function):
+
+        def checker(*args, **kwargs):
+            try:
+                result = function(*args, **kwargs)
+            except (exc_types) as exc:
+                print(f"We have a problems {exc}")
+            else:
+                print(f"No problems. Result - {result}")
+        return checker
+    return checker()
 
 
-
-
-
-
-
-
+@checker(NameError,TypeError,SyntaxError,ZeroDivisionError)
+def calculate(expression):
+    return eval(expression)
+calculate('2+2')
